fb978a38446a4e35e580c7c54a8f2886df7ac66d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-05-27 21:58:35 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,43 +1,82 @@
 import socket
-import pickle 
+import select
+import errno
 
-#socket is endpoint that receives data
+HEADER_LENGTH = 10
 
-HEADERSIZE = 10
+IP = "127.0.0.1"
+PORT = 1234
+my_username = input("Username: ")
 
-#AF_INET = IPV4
-#SOCK_STREAM = TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#lockal host, portnumber can be any 4-digit number
-s.connect((socket.gethostname(), 1234))
+# Create a socket
+# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Connect to a given ip and port
+client_socket.connect((IP, PORT))
 
-#accept the message that was send to us from the server
-while True: 
+# Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
+client_socket.setblocking(False)
 
-    #buffers for data stream
-    full_msg = b''
-    new_msg = True
+# Prepare username and header and send them
+# We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+client_socket.send(username_header + username)
 
-    while True: 
-        msg = s.recv(16)  
-        if new_msg:
-            print(f"new message length: {msg[:HEADERSIZE]}")
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
+while True:
 
-        full_msg += msg
-        
-        if len(full_msg) - HEADERSIZE == msglen:
-            print("full msg recvd")
-            print(full_msg[HEADERSIZE:])
-            d = pickle.loads(full_msg[HEADERSIZE:])
-            print(d)
+    # Wait for user to input a message
+    message = input(f'{my_username} > ')
 
-            #clear the buffers
-            full_msg = b''
-            new_msg = True
-    
-print(full_msg)
+    # If message is not empty - send it
+    if message:
 
+        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(message_header + message)
 
+    try:
+        # Now we want to loop over received messages (there might be more than one) and print them
+        while True:
+
+            # Receive our "header" containing username length, it's size is defined and constant
+            username_header = client_socket.recv(HEADER_LENGTH)
+
+            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+            if not len(username_header):
+                print('Connection closed by the server')
+                sys.exit()
+
+            # Convert header to int value
+            username_length = int(username_header.decode('utf-8').strip())
+
+            # Receive and decode username
+            username = client_socket.recv(username_length).decode('utf-8')
+
+            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
+
+            # Print message
+            print(f'{username} > {message}')
+
+    except IOError as e:
+        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
+        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+        # If we got different error code - something happened
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error: {}'.format(str(e)))
+            sys.exit()
+
+        # We just did not receive anything
+        continue
+
+    except Exception as e:
+        # Any other exception - something happened, exit
+        print('Reading error: '.format(str(e)))
+        sys.exit()
--- server.py
+++ server.py
@@ -1,44 +1,75 @@
 import socket
 import time
 import pickle
+import select
 
-#socket is endpoint that receives data
-
-HEADERSIZE = 10
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
 
 #AF_INET = IPV4
 #SOCK_STREAM = TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#lockal host, portnumber can be any 4-digit number
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#this shit allows you to recconect
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-#listening forever
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+#list of clients
+sockets_list = [server_socket]
+
+#dictionary of clients
+clients = {}
+
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+        
+        message_length = int(message_header.decode("utf-8").strip())
+        return {"header": message_header, "data": client_socket.recv( message_length)}
+    
+    except (ValueError,IOError) as err:
+        print(err)
+
 while True:
-    #accepting everyone
-    #storing socket object into clientsocket addres
-    #address is their ip address
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
+    # select accepts three parameters 1. things you want to read 2. things you want to write to 3. errors
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
-    '''
-    while True:
-        time.sleep(3)
-        msg = f"The time is! {time.time()}"
-        msg = f'{len(msg):<{HEADERSIZE}}' + msg
-        clientsocket.send(bytes(msg, "utf-8"))
-     
-    d = "Welcome to the server!"
-    msg = pickle.dumps(d)
-    msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
-    clientsocket.send(msg)
-    '''   
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
 
-    d = {1: "Hey", 2: "There"}
-    msg = pickle.dumps(d)
-    msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
-    clientsocket.send(msg)
+            user = receive_message(client_socket)
+            if user is False:
+                continue
 
-    #closing by the end
-    clientsocket.close()
+            sockets_list.append(client_socket)
+            clients[client_socket] = user
 
+            print(f"Accepted new conection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+
+        else:
+            message = receive_message(notified_socket)
+
+            if message is False:
+                print(f"Closed conection from {clients[notified_socket]['data'].decode('utf-8')}")
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+
+            user = clients[notified_socket]
+            print(f"Receieved message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+                    
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
+        
